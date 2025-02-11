7801d3f2e9427f74a40c0d0a40de0363620b8bfd
Akshat Rauthan <akshatrauthan9433@gmail.com>
2025-02-11 11:42:33 +0530

    Update city-routes.js

--- Flights-Service/src/routes/v1/city-routes.js
+++ Flights-Service/src/routes/v1/city-routes.js
@@ -6,28 +6,28 @@ const express = require('express');
 
 const router = express.Router();
 
-// /api/v1/city POST
+// /api/v1/cities POST
 router.post('/',
         CityMiddleware.validateCreateObject,
         CityController.createCity
 );
 
-// api/v1/city GET
+// api/v1/cities GET
 router.get('/',
         CityController.getCities
 );
 
-// api/v1/city/:id POST
+// api/v1/cities/:id POST
 router.get('/:id',
         CityController.getCity
 );
 
-// api/v1/city/:id DELETE
+// api/v1/cities/:id DELETE
 router.delete('/:id',
         CityController.destroyCity
 )
 
-// api/v1/city/:id PATCH
+// api/v1/cities/:id PATCH
 router.patch('/:id',
         CityMiddleware.validateUpdateObject,
         CityController.updateCity
